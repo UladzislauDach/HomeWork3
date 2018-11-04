f08bdd142b545b61964ede0179ee39936ba23a16
uladzislaudach <vladzayac1@gmail.com>
2018-11-04 21:08:59 +0200

    с

--- src/apples.java
+++ src/apples.java
@@ -1,20 +1,20 @@
 public class apples {
-
-    static int pApples = 10; // яблоки пети
-    static int vApples; // яблоки васи
-    static int applesSumm = 15; // всего яблок
+    static int applesPete = 10; // яблоки пети
+    static int applesYasa; // яблоки васи
+    static int applesTotal = 15; // всего яблок
 
     public static void main(String[] args) {
-
-        //vApples = applesSumm - pApples; // задание 1
-        System.out.println("У васи " + calculate(applesSumm, pApples) + " яблок");
-
+        applesYasa = calculate(applesPete, applesTotal);
+        print(applesYasa);
     }
 
-    public static int calculate(int a, int b) {
-        int vApples = a - b;
+    public static int calculate(int applesPete, int applesTotal) {
+        int vApples = applesTotal - applesPete;
         return vApples;
+    }
 
+    public static void print(int z) {
+        System.out.println("У Васи " + z + " яблок");
 
     }
 }
